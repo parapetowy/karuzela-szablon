3fc048b17ffe80feef2c585e0e8114fa1abfaf69
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-07-05 20:48:59 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 "use strict";
 
 
-var templateItem = document.getElementById('slajd-item').innerHTML;
+var templateItem = document.getElementById('slajd-template').innerHTML;
 
 Mustache.parse(templateItem);
 
 var listItems = '';
 
-for(var i = 0; i < slajdData.length; i++){
-		console.log(slajdData);
-		listItems += Mustache.render(templateItem, slajdData[i]);
-	}
+for (var i = 0; i < slajdData.length; i++) {
+    console.log(slajdData);
+    listItems += Mustache.render(templateItem, slajdData[i]);
+}
 
 
 tutaj.insertAdjacentHTML('beforeend', listItems);
 
-var flkty = new Flickity( '.main-carousel', {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  hash: true,
- });
+var flkty = new Flickity('.main-carousel', {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    hash: true,
+});
 
 var progressBar = document.querySelector('.progress-bar')
 
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
+flkty.on('scroll', function(progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
 
-var buttonReset = document.querySelector('.buttonReset');
+var buttonReset = document.querySelector('.buttonreset');
 
-buttonReset.addEventListener( 'click', function( event ) {
-   flkty.select( 0 );
+buttonReset.addEventListener('click', function(event) {
+    flkty.select(0);
 });
